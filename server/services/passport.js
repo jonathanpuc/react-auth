f021ac114ef529442a20a690a5bfbe1c028d6628
Jonathan <jonathancoder77@outlook.com>
2018-04-01 10:48:16 +1000

    create auth middleware

---
+++ server/services/passport.js
@@ -0,0 +1,32 @@
+const passport = require('passport');
+const User = require('../models/user');
+const config = require('../config');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+
+// Setup options for JWT Strategy
+const jwtOptions = {
+    jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+    secretOrKey: config.secret
+};
+
+// Create JWT strategy
+const jwtLogin = new JwtStrategy(jwtOptions, function (payload, done) {
+    // See if user ID in payload exists in database
+    // if does call done with that user
+    // else call done without a user object
+    User.findById(payload.sub, function (err, user) {
+        if (err) { return done(err, false); }
+
+        if (user) {
+            done(null, user);
+        } else {
+            done(null, false)
+        }
+    });
+});
+
+
+// Tell passport ot use this strategy
+passport.use(jwtLogin);
