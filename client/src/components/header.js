ff6a6a828f274fbf36a2afeed21684fbf7ce762c
Jonathan <jonathancoder77@outlook.com>
2018-04-02 20:37:39 +1000

    add auth links logic to header componenet

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,14 +1,41 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
+
 
 class Header extends Component {
     render() {
+
+        const renderLinks = () => {
+            if (this.props.authenticated) {
+                return (
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/signout">Sign Out</Link>
+                    </li>
+                )
+            } else {
+                return [
+                    <li className="nav-item" key={1}>
+                        <Link className="nav-link" to="/signin">Sign In</Link>
+                    </li>,
+                    <li className="nav-item" key={2}>
+                        <Link className="nav-link" to="/signup">Sign Up</Link>
+                    </li>
+                ]
+            }
+        }
         return (
             <nav className="navbar navbar-light">
+                <Link to="/" className="navbar-brand">Redux Auth</Link>
                 <ul className="nav navbar-nav">
-                    <li className="nav-item">
-                        Sign in</li></ul></nav>
+                    {renderLinks()}
+                </ul></nav>
         )
     }
 }
 
-export default Header;
+const mapStateToProps = (state) => ({
+    authenticated: state.auth.authenticated
+})
+
+export default connect(mapStateToProps, null)(Header);
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,6 +1,10 @@
 import { AUTH_USER, UNAUTH_USER, AUTH_ERROR } from '../actions/types';
 
-export default function (state = {}, action) {
+const initialState = {
+    authenticated: false
+}
+
+export default function (state = initialState, action) {
     switch (action.type) {
         case AUTH_USER:
             return { ...state, authenticated: true };
